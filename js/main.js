c19bae8d1c8300a4899eef536e147f1457d7b7d3
Serik <jetheez@yandex.ru>
2020-08-21 19:55:30 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("a.scrollto").click(function () {
+        var elementClick = '#'+$(this).attr("href").split("#")[1]
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $('.ag-comments-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      arrows: false,
+      dots: true,
+    });
+});
