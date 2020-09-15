ca4f91903bf5c479376500052a9dea89d2cee291
Serik <jetheez@yandex.ru>
2020-09-15 22:37:37 +0500

    creat modal window

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
-$(document).ready(function() {
-    $("a.ag-header-btn").click(function () {
-        var elementClick = '#'+$(this).attr("href").split("#")[1]
-        var destination = $(elementClick).offset().top;
-        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
-        return false;
-    });
+// --> My Script Begin
+
+let openModal = document.querySelector('.ag-modal'),
+openModalBtn = document.querySelectorAll('.ag-food-overlay-link'),
+modalCloseBtn = document.querySelector('.ag-modal__close'),
+body = document.querySelector('body');
+
+
+for (let i = 0; i < openModalBtn.length; i++) {
+  openModalBtn[i].addEventListener('click', () => {
+    openModal.style.opacity = '5';
+    openModal.style.visibility = 'visible';
+    body.style.overflow = 'hidden';
+  });
+}
+
+modalCloseBtn.addEventListener('click', () => {
+  openModal.style.opacity = '0';
+  openModal.style.visibility = 'hidden';
+  body.style.overflow = 'auto';
 });
+
+window.onclick = function(event) {
+  if (event.target == feedBack) {
+    openModal.style.opacity = '0';
+    openModal.style.visibility = 'hidden';
+    body.style.overflow = 'auto';
+  }
+}
+
+//  --> My Script End
+
+// $(document).ready(function() {
+//     $("a.ag-header-btn").click(function () {
+//         var elementClick = '#'+$(this).attr("href").split("#")[1]
+//         var destination = $(elementClick).offset().top;
+//         jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+//         return false;
+//     });
+// });
 
 $(document).ready(function(){
     $('.ag-comments-slider').slick({
